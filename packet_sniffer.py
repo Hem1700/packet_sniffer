547a47a4bb2d82d8f4ec40b53bc14d427ed5bafb
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-05 14:22:24 +0530

    Arguments added

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -2,7 +2,17 @@
 
 import scapy.all as scapy
 from scapy.layers import http
+import argparse
 # from scapy_http import *
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i" , "--interface", dest="interface", help="Interface on which the target computer is on")
+    options = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, type --help for more info")
+    return options
 
 def snif(interfce):
     scapy.sniff(iface=interfce, store=False, prn=process_sniffed_packet)
@@ -29,6 +39,6 @@ def process_sniffed_packet(packet):
 
 
 
-
-snif("wlan0")
+options = get_arguments()
+snif(options.interface)
 
